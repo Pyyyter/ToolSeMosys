95466460352b885e3882834ee470a99307a9b2df
Pedro Ribeiro Filho <97203584+Pyyyter@users.noreply.github.com>
2024-03-05 19:01:42 +0000

    Feat : Added the CreateTimeslice function

--- Carnaval.py
+++ Carnaval.py
@@ -22,6 +22,9 @@ def delimita_fim_da_tabela(linha):
 		return True
 
 def ler_tabela(nome_arquivo, nome_tabela):
+	"""
+	Função que recebe o nome de um arquivo usando o nome de uma tabela e retorna a mesma em forma de lista de listas ( matriz ).
+	"""
 	tabela = []
 	encontrou_tabela = False
 	try:
@@ -45,71 +48,80 @@ def ler_tabela(nome_arquivo, nome_tabela):
 			print("O arquivo especificado não foi encontrado.")
 			return None	
 
-def remover_primeira_coluna(matriz):
-	return [linha[1:] for linha in matriz]
+def remover_primeira_coluna(tabela):
+	return [linha[1:] for linha in tabela]
 
-def remover_primeira_linha(matriz):
-	return matriz[1:]
+def remover_primeira_linha(tabela):
+	return tabela[1:]
 
-def reduzir_tabela_com_divisao(matriz_original, novo_tamanho):
-	if (len(matriz_original)-1) % novo_tamanho != 0:
-		return print("A matriz não é redutível para esse número de linhas")
+def reduzir_tabela_com_media(tabela_original, novo_tamanho):
+	"""
+	Função que recebe uma tabela e um novo tamanho e retorna uma nova tabela com o tamanho especificado, fazendo a soma das linhas que foram agrupadas para a redução ocorrer.
+	"""
+	if (len(tabela_original)-1) % novo_tamanho != 0:
+		return print("A tabela não é redutível para esse número de linhas")
 	
-	matriz_filtrada = remover_primeira_linha(remover_primeira_coluna(matriz_original))
-	nova_matriz = []
+	tabela_filtrada = remover_primeira_linha(remover_primeira_coluna(tabela_original))
+	nova_tabela = []
 
 	# Fator é a quantidade de linhas antigas serão combinadas para que uma nova seja criada
-	fator = int(len(matriz_filtrada)/novo_tamanho)
+	fator = int(len(tabela_filtrada)/novo_tamanho)
 
-	# Povoamento da matriz nova
+	# Povoamento da tabela nova
 	for i in range(novo_tamanho):
-		nova_matriz.append([])
-		for j in range(len(matriz_original[1])-1):
-			nova_matriz[i].append(1)
+		nova_tabela.append([])
+		for j in range(len(tabela_original[1])-1):
+			nova_tabela[i].append(1)
 
 	for i in range(novo_tamanho):
-		for j in range(len(matriz_filtrada[1])):
+		for j in range(len(tabela_filtrada[1])):
 			soma = 0
 			for k in range(fator):
-				soma += float(matriz_filtrada[i*fator-k][j])
-			nova_matriz[i][j] = soma/fator
+				soma += float(tabela_filtrada[i*fator-k][j])
+			nova_tabela[i][j] = soma/fator
 
-	nova_matriz.insert(0,matriz_original[0])
+	nova_tabela.insert(0,tabela_original[0])
 
-	for i in range(len(nova_matriz)):
-		nova_matriz[i].insert(0,matriz_original[i][0])
-	return nova_matriz
+	for i in range(len(nova_tabela)):
+		nova_tabela[i].insert(0,tabela_original[i][0])
+	return nova_tabela
 
-def reduzir_tabela_com_soma(matriz_original, novo_tamanho):
-	if (len(matriz_original)-1) % novo_tamanho != 0:
-		return print("A matriz não é redutível para esse número de linhas")
+def reduzir_tabela_com_soma(tabela_original, novo_tamanho):
+	"""
+	Função que recebe uma tabela e um novo tamanho e retorna uma nova tabela com o tamanho especificado, fazendo a soma das linhas que foram agrupadas para a redução ocorrer.
+	"""
+	if (len(tabela_original)-1) % novo_tamanho != 0:
+		return print("A tabela não é redutível para esse número de linhas")
 	
-	matriz_filtrada = remover_primeira_linha(remover_primeira_coluna(matriz_original))
-	nova_matriz = []
+	tabela_filtrada = remover_primeira_linha(remover_primeira_coluna(tabela_original))
+	nova_tabela = []
 
 	# Fator é a quantidade de linhas antigas serão combinadas para que uma nova seja criada
-	fator = int(len(matriz_filtrada)/novo_tamanho)
+	fator = int(len(tabela_filtrada)/novo_tamanho)
 
-	# Povoamento da matriz nova
+	# Povoamento da tabela nova
 	for i in range(novo_tamanho):
-		nova_matriz.append([])
-		for j in range(len(matriz_original[1])-1):
-			nova_matriz[i].append(1)
+		nova_tabela.append([])
+		for j in range(len(tabela_original[1])-1):
+			nova_tabela[i].append(1)
 
 	for i in range(novo_tamanho):
-		for j in range(len(matriz_filtrada[1])):
+		for j in range(len(tabela_filtrada[1])):
 			soma = 0
 			for k in range(fator):
-				soma += float(matriz_filtrada[i*fator-k][j])
-			nova_matriz[i][j] = soma
+				soma += float(tabela_filtrada[i*fator-k][j])
+			nova_tabela[i][j] = soma
 
-	nova_matriz.insert(0,matriz_original[0])
+	nova_tabela.insert(0,tabela_original[0])
 
-	for i in range(len(nova_matriz)):
-		nova_matriz[i].insert(0,matriz_original[i][0])
-	return nova_matriz
+	for i in range(len(nova_tabela)):
+		nova_tabela[i].insert(0,tabela_original[i][0])
+	return nova_tabela
 
-def substituir_tabela(caminho_arquivo, nova_matriz, nome_tabela):
+def substituir_tabela(caminho_arquivo, nova_tabela, nome_tabela):
+	"""
+	Função que recebe a tabela nova, o nome da tabela e o caminho do arquivo e substitui a tabela no arquivo pela tabela passada por parâmetro.
+	"""
 	try:
 		with open(caminho_arquivo, 'r') as arquivo:
 			linhas = arquivo.readlines()
@@ -125,10 +137,10 @@ def substituir_tabela(caminho_arquivo, nova_matriz, nome_tabela):
 				indice_fim = i
 				break  # Parar de procurar
 
-		# Substituir a tabela no arquivo pelo conteúdo da nova matriz
+		# Substituir a tabela no arquivo pelo conteúdo da nova tabela
 		if indice_inicio and indice_fim :
 			linhas_substituidas = linhas[:indice_inicio + 1]  # Mantém as linhas antes da tabela
-			for linha in nova_matriz:
+			for linha in nova_tabela:
 				linhas_substituidas.append(' '.join(map(str, linha)) + '\n') 
 			linhas_substituidas.extend(linhas[indice_fim:])  
 
@@ -140,10 +152,10 @@ def substituir_tabela(caminho_arquivo, nova_matriz, nome_tabela):
 	except:
 		print("Erro na função substituir tabela.")
 
-'''
-Retorna uma lista de strings com o nome das tabelas que contém o parâmetro especificado.
-'''
 def lerParams(param, caminho_arquivo):
+	'''
+	Retorna uma lista de strings com o nome das tabelas que estão contidas no parâmetro especificado.
+	'''
 	nome_das_tabelas = []
 	
 	encontrou_tabela = False
@@ -159,13 +171,27 @@ def lerParams(param, caminho_arquivo):
 									nome_das_tabelas.append(linha)
 	return nome_das_tabelas
 
-def cortar_matriz(matriz, p, q):
-	if p > len(matriz) or q > len(matriz[-1]):
-		print("A nova matriz é maior que a matriz original.")
+def cortar_tabela(tabela, p, q):
+	"""
+	Função que literalmente corta uma tabela de tamamho MxN para PxQ, jogando fora as linhas e colunas excedentes.
+	"""
+	if p > len(tabela) or q > len(tabela[-1]):
+		print("A nova tabela é maior que a tabela original.")
 		return None
 
-	nova_matriz = [linha[:q] for linha in matriz[:p]]
-	return nova_matriz
+	nova_tabela = [linha[:q] for linha in tabela[:p]]
+	return nova_tabela
+
+def gerar_timeslices(seasons, daytypes, dailytimebrackets):
+	"""
+	Função que recebe o caminho do arquivo, o nome da tabela e o novo tamanho e retorna a tabela com o novo tamanho.
+	"""
+	timeslices = []
+	for season in seasons:
+		for daytype in daytypes:
+			for dailytimebracket in dailytimebrackets:
+				timeslices.append("S"+str(season)+"D"+str(daytype)+"T"+str(dailytimebracket))
+	return timeslices
 #endregion
 
 caminho_arquivo = 'Atlantis_base/atlantis_v2copy.txt'
@@ -174,16 +200,18 @@ novo_tamanho = 1
 nome_das_tabelas = lerParams(nome_param, caminho_arquivo)
 tabelas_diferentes = ['Conversionld', 'Conversionls', 'Conversionlh']
 
-try :
-	for nome_da_tabela in nome_das_tabelas:
-		substituir_tabela(caminho_arquivo, reduzir_tabela_com_soma(ler_tabela(caminho_arquivo, nome_da_tabela), novo_tamanho), nome_da_tabela)
-except:
-	pass
+# try :
+# 	for nome_da_tabela in nome_das_tabelas:
+# 		substituir_tabela(caminho_arquivo, reduzir_tabela_com_soma(ler_tabela(caminho_arquivo, nome_da_tabela), novo_tamanho), nome_da_tabela)
+# except:
+# 	pass
+timeslices = gerar_timeslices([1,2,3,4], [1,2], [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])
+print(timeslices)
 
 # for tabelas_diferente in tabelas_diferentes:
 # 	if tabelas_diferente == 'Conversionld':
-# 		substituir_tabela(caminho_arquivo, reduzir_matriz(ler_tabela(caminho_arquivo,tabelas_diferente),2,2), tabelas_diferente)
+# 		substituir_tabela(caminho_arquivo, reduzir_tabela(ler_tabela(caminho_arquivo,tabelas_diferente),2,2), tabelas_diferente)
 # 	if tabelas_diferente == 'Conversionls':
-# 		substituir_tabela(caminho_arquivo, reduzir_matriz(ler_tabela(caminho_arquivo, tabelas_diferente), 2, 2), tabelas_diferente)
+# 		substituir_tabela(caminho_arquivo, reduzir_tabela(ler_tabela(caminho_arquivo, tabelas_diferente), 2, 2), tabelas_diferente)
 # 	if tabelas_diferente == 'Conversionlh':
-# 		substituir_tabela(caminho_arquivo, reduzir_matriz(ler_tabela(caminho_arquivo, tabelas_diferente), 2, 2), tabelas_diferente)
+# 		substituir_tabela(caminho_arquivo, reduzir_tabela(ler_tabela(caminho_arquivo, tabelas_diferente), 2, 2), tabelas_diferente)
